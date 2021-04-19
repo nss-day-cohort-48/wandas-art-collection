29b7e4ef853c2614e7dd4dcf840eee760fd0a3bc
Steve Brownlee <chortlehoort@gmail.com>
2021-04-19 11:40:48 -0500

    Students built a solid find function for the art

--- artworks.js
+++ artworks.js
@@ -1,16 +1,6 @@
-/**
- * The variable 'artCollection'
- * holds the value of an array
- * of objects.
- **/
-
 const artCollection = [
     {
-        id: 1 /** Number */
-    /**
-     * The id is the unique identifier.
-     * Also known as the primary key
-     **/,
+        id: 1, /** Number */
         title: "Starry Night" /** String */,
         artistName: "Van Gogh" /** String */,
         price: 400000.0 /** Number */,
@@ -31,6 +21,7 @@ const artCollection = [
     }
 ]
 
+
 const davidSculpture = {
     id: 3,
     title: "Sculpture of David",
@@ -44,6 +35,33 @@ const davidSculpture = {
 
 artCollection.push(davidSculpture)
 
+const monaLisa = {
+    id: 4,
+    title: "Mona Lisa",
+    artistName: "Leonardo da Vinci",
+    price: 5000000.33,
+    typeOfMedium: "Oil",
+    inStock: true,
+    notes: "Meh",
+    dateAquired: "April 11, 2021",
+}
+
+artCollection.push(monaLisa)
+
+const counterfeitDavidSculpture = {
+    id: 5,
+    title: "Sculpture of David",
+    artistName: "Jeffangelo",
+    price: 10000.99,
+    typeOfMedium: "Concrete",
+    inStock: true,
+    notes: "Much heavier than I thought",
+    dateAquired: "January 18, 2021",
+}
+
+artCollection.push(counterfeitDavidSculpture)
+
+
 
 /**
  * In order to access the data inside the array,
@@ -51,21 +69,47 @@ artCollection.push(davidSculpture)
  * A for..of loop is used to iterate an array.
  **/
 
+
+/**
+ * The variable 'artwork' holds the value
+ * of each individual item in the array,
+ * and it only exists inside these curly braces.
+ **/
 for (const artwork of artCollection) {
-    /**
-     * The variable 'artwork' holds the value
-     * of each individual item in the array,
-     * and it only exists inside these curly braces.
-     **/
+
+    // Wanda double's the market value for sale on the Dark Web
     artwork.price = artwork.price * 2
 
+    // Wanda doesn't want potential clients to see items that have already sold
     if (artwork.inStock) {
-        console.log(`The artwork ${artwork.title} is valued at $${artwork.price}.`)
+        // console.log(`The artwork ${artwork.title} is valued at $${artwork.price}.`)
     }
-    /**
-     * We use dot notation to
-     * access the value of the properties
-     * on each artwork object.
-     */
 }
+
+
+// Wanda wants to be able to find a particular piece of art by its title
+const findArtpiece = (title) => {
+    // Track if the piece was found. Assume it hasn't by default.
+    let titlesFound = []
+
+    // Look at each art object
+    for (const art of artCollection) {
+
+        // Does the current object's title property value equal the parameter
+        if (title === art.title) {
+            titlesFound.push(art)
+        }
+    }
+
+    // Loop done.... now what?
+    if (titlesFound.length > 0) {
+        console.log(titlesFound)
+    }
+    else {
+        console.log(`Could not find ${title}`)
+    }
+}
+
+findArtpiece("Sculpture of David")
+
 
