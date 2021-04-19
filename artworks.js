528c52d8113cd96abc2615d214e0a0afe82c666e
Steve Brownlee <chortlehoort@gmail.com>
2021-04-19 10:44:30 -0500

    Adding new stolen object to inventory

--- artworks.js
+++ artworks.js
@@ -5,31 +5,44 @@
  **/
 
 const artCollection = [
-  {
-    id: 1 /** Number */
+    {
+        id: 1 /** Number */
     /**
      * The id is the unique identifier.
      * Also known as the primary key
      **/,
-    title: "Starry Night" /** String */,
-    artistName: "Van Gogh" /** String */,
-    price: 400000.0 /** Number */,
-    typeOfMedium: "Oils" /** String */,
-    inStock: true /** Boolean */,
-    notes: "This is a dope painting." /** String */,
-    dateAquired: "January 01, 2020" /** String */,
-  },
-  {
-    id: 2,
-    title: "The Persistence Of Memory",
-    artistName: "Salvido Dali",
-    price: 2.0,
-    typeOfMedium: "Oils",
-    inStock: false,
-    notes: "This one's aaaight!",
-    dateAquired: "December 03, 1924",
-  }
+        title: "Starry Night" /** String */,
+        artistName: "Van Gogh" /** String */,
+        price: 400000.0 /** Number */,
+        typeOfMedium: "Oils" /** String */,
+        inStock: true /** Boolean */,
+        notes: "This is a dope painting." /** String */,
+        dateAquired: "January 01, 2020" /** String */,
+    },
+    {
+        id: 2,
+        title: "The Persistence Of Memory",
+        artistName: "Salvido Dali",
+        price: 2.0,
+        typeOfMedium: "Oils",
+        inStock: false,
+        notes: "This one's aaaight!",
+        dateAquired: "December 03, 1924",
+    }
 ]
+
+const davidSculpture = {
+    id: 3,
+    title: "Sculpture of David",
+    artistName: "Michaelangelo",
+    price: 1000000.99,
+    typeOfMedium: "Marble",
+    inStock: true,
+    notes: "Much heavier than I thought",
+    dateAquired: "April 18, 2021",
+}
+
+artCollection.push(davidSculpture)
 
 
 /**
@@ -39,16 +52,20 @@ const artCollection = [
  **/
 
 for (const artwork of artCollection) {
-  /**
-   * The variable 'artwork' holds the value
-   * of each individual item in the array,
-   * and it only exists inside these curly braces.
-   **/
-  console.log(`The artwork ${artwork.title} is valued at $${artwork.price}.`)
-  /**
-   * We use dot notation to
-   * access the value of the properties
-   * on each artwork object.
-   */
+    /**
+     * The variable 'artwork' holds the value
+     * of each individual item in the array,
+     * and it only exists inside these curly braces.
+     **/
+    artwork.price = artwork.price * 2
+
+    if (artwork.inStock) {
+        console.log(`The artwork ${artwork.title} is valued at $${artwork.price}.`)
+    }
+    /**
+     * We use dot notation to
+     * access the value of the properties
+     * on each artwork object.
+     */
 }
 
